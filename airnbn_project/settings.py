52188dcb5fc84e51cd1bf8cbcd0550c8634bafc6
tranVyTDT <89985585+VyTruongTDTU@users.noreply.github.com>
2025-08-20 22:27:17 +0700

    deploy

--- airnbn_project/settings.py
+++ airnbn_project/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-hd(#7fj7*gw8+i+!b24j82q#^6($bbyr_r_*#heyw*)vs!tfzl'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["nguyenvietanhnov.com", ".vercel.app", "localhost", "127.0.0.1"]
 
