10703099ed546d86495103447490250977bd7d0f
tranVyTDT <89985585+VyTruongTDTU@users.noreply.github.com>
2025-08-20 22:15:54 +0700

    deploy

--- build_files.sh
+++ build_files.sh
@@ -9,10 +9,10 @@ pip install -r requirements.txt
 
 # Collect static files
 echo "Collecting static files..."
-python manage.py collectstatic --noinput
+python3.9 manage.py collectstatic --noinput
 
 # Run migrations
 echo "Running migrations..."
-python manage.py migrate --noinput
+python3.9 manage.py migrate --noinput
 
 echo "Build completed successfully!"
